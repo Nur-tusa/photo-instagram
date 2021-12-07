7e042eeb6726e2eeca204a1bce64980e7cd36ec8
Nur tusa <nur.tusa@student.moringaschool.com>
2021-12-07 19:44:54 +0300

    adding my project to heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -24,6 +24,7 @@ from decouple import config,Csv
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
+
 # development
 if config('MODE')=="dev":
    DATABASES = {
